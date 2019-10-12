e64d97165c5a9f16ce0352056669e5be7d4036d2
Karthik K <karthiknetworklink+code@gmail.com>
2019-10-13 02:03:01 +0530

    docs: upgrade

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 AltosCode, LLC
+// Copyright (c) 2019 MalvaHQ
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-19 AltosCode, LLC
+// Copyright (c) 2018-19 MalvaHQ
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
