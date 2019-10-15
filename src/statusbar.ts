b3170548ebe0543bd69f3f56401a5e8cb149d776
Karthik K <karthiknetworklink+code@gmail.com>
2019-10-15 15:17:31 +0530

    fix: list sass watchers

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -4,23 +4,11 @@
 // https://opensource.org/licenses/MIT
 'use strict';
 import * as vscode from 'vscode';
-import * as common from 'dartsass-plugin-common';
 
-let myStatusBarItem: vscode.StatusBarItem;
+export let myStatusBarItem: vscode.StatusBarItem;
 
 export function createStatusBarItem(subscriptions: vscode.Disposable[], viewWatchersCmd: string) {
 	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	myStatusBarItem.command = viewWatchersCmd;
 	subscriptions.push(myStatusBarItem);
-}
-
-export function updateStatusBar(watcher: common.Watcher) {
-    const watchList: Map<string, number> = watcher.GetWatchList();
-    const numWatchers: number = watchList.size;
-    if (numWatchers > 0) {
-        myStatusBarItem.text = `Sass Watchers: ${numWatchers}`;
-        myStatusBarItem.show();
-    } else {
-        myStatusBarItem.hide();
-    }
 }
--- src/watcher.ts
+++ src/watcher.ts
@@ -6,9 +6,21 @@
 import * as vscode from 'vscode';
 import * as common from 'dartsass-plugin-common';
 import { getProjectRoot } from './doc';
+import { myStatusBarItem } from './statusbar';
 
 const watcher = new common.Watcher();
 
+
+export function updateStatusBar(watcher: common.Watcher) {
+    const watchList: Map<string, number> = watcher.GetWatchList();
+    const numWatchers: number = watchList.size;
+    if (numWatchers > 0) {
+        myStatusBarItem.text = `Sass Watchers: ${numWatchers}`;
+        myStatusBarItem.show();
+    } else {
+        myStatusBarItem.hide();
+    }
+}
 export function watchDirectory(_srcdir: vscode.Uri, config: common.CompilerConfig, _log: common.ILog) {
     const uri = getProjectRoot(_srcdir);
     if (!uri) {
@@ -17,9 +29,11 @@ export function watchDirectory(_srcdir: vscode.Uri, config: common.CompilerConfi
     const projectRoot = uri.fsPath;
     watcher.Watch(_srcdir.fsPath, projectRoot, config, _log).then(
         value => {
-            vscode.window.showInformationMessage(`Watching Directory ${_srcdir.fsPath} - ${value}`);
+            vscode.window.showInformationMessage(`Watching Directory ${_srcdir.fsPath}`);
+            updateStatusBar(watcher);
         },
         err => {
+            _log.appendLine(typeof err);
             vscode.window.showErrorMessage(`${err}`);
         }
     );
