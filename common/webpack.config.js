9ba423f15b5e6c827dbf1c02a7940dfed732a4f5
Karthik K <karthiknetworklink@gmail.com>
2022-07-04 12:22:33 +0530

    fix: format

--- common/webpack.config.js
+++ common/webpack.config.js
@@ -1,45 +1,54 @@
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
+const { IgnorePlugin } = require('webpack');
+
+const optionalPlugins = [];
+if (process.platform !== "darwin") {
+  optionalPlugins.push(new IgnorePlugin({ resourceRegExp: /^fsevents$/ }));
+}
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-  target: 'node', // vscode extensions run in a Node.js-context  -> https://webpack.js.org/configuration/node/
-  entry: './src/index.ts',
+  target: "node", // vscode extensions run in a Node.js-context  -> https://webpack.js.org/configuration/node/
+  entry: "./src/index.ts",
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    library: 'DartSassPluginCommon',
-    umdNamedDefine: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "DartSassPluginCommon",
+    umdNamedDefine: true,
   },
-  devtool: 'source-map',
+  plugins: [
+      ...optionalPlugins,
+  ],
+  devtool: "source-map",
   externals: {
-    vscode: 'commonjs' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: "commonjs", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.node$/,
-        loader: 'node-loader',
+        loader: "node-loader",
       },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  }
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
 };
 module.exports = config;
